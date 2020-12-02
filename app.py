2d908bdf98ad292c14bc0a0596898e75a109962b
Olivier Kessler <olivier.kessler@protonmail.com>
2020-12-02 22:32:26 +0100

    Finalize blog structure

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_swagger import swagger
+
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+from models import Articles
 
 #Home action
 @app.route('/', methods=['GET'])
 def hello():
-    return render_template('home.html')
+    articles = Articles.query.all();
+    return render_template('home.html', articles=articles)
 
 @app.route('/api-spec', methods=['GET'])
 def spec():
