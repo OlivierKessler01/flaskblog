b431fe7f2efdd88f90734028e69747f48683c804
Olivier Kessler <olivier.kessler@protonmail.com>
2020-12-03 18:14:50 +0100

    Add Influx insert

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,10 @@ ENV FLASK_RUN_HOST=0.0.0.0
 ENV FLASK_ENV=development
 ENV DATABASE_URL=postgresql://postgres:postgres@db:5432/postgres
 ENV APP_SETTINGS=config.DevelopmentConfig
+ENV INFLUXDB_USERNAME=admin
+ENV INFLUXDB_PASSWORD=admin
 
-
-RUN apk add --no-cache gcc musl-dev linux-headers
+RUN apk add --no-cache gcc musl-dev linux-headers bash
 COPY requirements.txt requirements.txt
 RUN apk add postgresql-dev gcc python3-dev musl-dev
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_swagger import swagger
+from influxdb import InfluxDBClient
+from datetime import datetime
 
 import os
 
@@ -8,17 +9,33 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+influxdb_client = InfluxDBClient(host='influxdb',port=8086,username=os.environ['INFLUXDB_USERNAME'],password=os.environ['INFLUXDB_PASSWORD'])
+influxdb_client.create_database('pyexample')
+influxdb_client.switch_database('pyexample')
+
 from models import Articles
 
 #Home action
 @app.route('/', methods=['GET'])
 def hello():
+    json_body = [
+        {
+            "measurement": "cpu_load_short",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "fields": {
+                "Int_value": 1
+            }
+        }
+    ]
+
+    influxdb_client.write_points(json_body)
+
     articles = Articles.query.all();
     return render_template('home.html', articles=articles)
-
-@app.route('/api-spec', methods=['GET'])
-def spec():
-    return jsonify(swagger(app))
 
 @app.route('/<name>', methods=['GET'])
 def hello_name(name):
