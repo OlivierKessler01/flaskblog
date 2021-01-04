b872d6be91ed72029a505d797e68e0de8fde90a6
Olivier kessler <olivier.kessler@protonmail.com>
2021-01-04 10:57:15 +0100

    Add article modification form

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ def hello():
     
     return render_template('home.html', articles=articles)
 
+
+@app.route('/update_article_form/<id>', methods=['GET'])
+def update_article_form(id):
+    article = mongo.db.Articles.find_one({'_id' : ObjectId(escape(id))})
+    if article:
+        return render_template('update_article.html', article=article)
+    else:
+        return jsonify(request="Article doesn't exist")
+    
 #Renders a form to delete an article
 @app.route('/delete_article_form', methods=['GET'])
 def delete_article_form():
@@ -78,7 +87,7 @@ def get_all_articles():
 #Rest API Article, Get one article by Id
 @app.route('/article/<id>', methods=['GET'])
 def get_article(id):
-    article = mongo.db.Articles.find_one({'_id' : escape(id)})
+    article = mongo.db.Articles.find_one({'_id' : ObjectId(escape(id))})
     
     if article:
         return jsonify("article found")
@@ -109,10 +118,29 @@ def post_article():
     else:
         return jsonify(request="fail : bad authentication") 
 
-#Rest API Article, modify one article
-#@app.route('/article', methods=['PUT'])
-#def put_article():
-#    return jsonify(request="fail")
+#Rest API Article, update one article
+@app.route('/update/article', methods=['PUT', 'POST'])
+def update_article():
+    id_article = escape(request.form['id_article'])
+
+    if id_article != '':
+        if(escape(request.form['database_username']) == os.environ['MONGO_USERNAME'] and escape(request.form['database_password']) == os.environ['MONGO_PASSWORD']):
+            result = mongo.db.Articles.update_one(
+                {'_id' : ObjectId(id_article)},
+                { "$set" : 
+                    {'content' : escape(request.form['content_article'])}
+                }    
+            )
+            if result.modified_count > 0:    
+                return jsonify(request="Success")
+            else:
+                return jsonify(request="Failed to update the article")
+        else:
+            return jsonify(request="fail: bad authentication")
+    else:
+        return jsonify(request="fail: Article id missing")
+
+
 
 #Rest API Article, delete one article
 @app.route('/delete/article', methods=['DELETE', 'POST'])
